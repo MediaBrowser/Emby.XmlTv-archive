84adfff10eddd06db356e5e8ffbfceff0fd3dad7
Alex Stevens <ads@chubbymusic.co.uk>
2016-06-29 14:05:26 +0100

    Change to reset the dates coming out to not have the Kind value set

--- Emby.XmlTv/Classes/XmlTvReader.cs
+++ Emby.XmlTv/Classes/XmlTvReader.cs
@@ -834,7 +834,7 @@ namespace Emby.XmlTv.Classes
                 DateTimeOffset parsedDateTime;
                 if (DateTimeOffset.TryParseExact(standardDate, "yyyyMMddHHmmss zzz", CultureInfo.CurrentCulture, DateTimeStyles.None, out parsedDateTime))
                 {
-                    result = parsedDateTime.LocalDateTime;
+                    result = new DateTime(parsedDateTime.LocalDateTime.Ticks);
                 }
                 else
                 {
