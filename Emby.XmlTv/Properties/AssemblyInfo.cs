37bacf41ff250c8b51766da37f94168cdf1e3c1c
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-11-03 14:33:26 -0400

    target .netstandard 1.0

--- Emby.XmlTv/Properties/AssemblyInfo.cs
+++ Emby.XmlTv/Properties/AssemblyInfo.cs
@@ -19,9 +19,6 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0d023565-5942-4d79-9098-a1b4b6665a40")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
